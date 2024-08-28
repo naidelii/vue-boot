102bcbbead149dd246f14d7c71c0cea69dee1bf5
naidelii <naidelii@qq.com>
2024-08-28 11:25:06 +0800

    fix:高亮有时候不生效

--- src/components/TreeSelect/index.vue
+++ src/components/TreeSelect/index.vue
@@ -1,11 +1,18 @@
 <template>
   <div class="tree-select">
-    <!-- 使用 el-select 作为下拉框 -->
+    <!-- 使用 el-select 作为下拉框，绑定 model 为 label -->
     <el-select ref="select" v-model="label" :placeholder="placeholder">
-      <!-- 自定义 el-option，将 el-tree 作为下拉内容 -->
+      <!-- 自定义 el-option，将 el-tree 作为下拉内容显示 -->
       <el-option class="tree-select-option" value="">
         <!-- 使用 el-tree 显示树形结构数据 -->
-        <el-tree ref="tree" :data="treeData" :props="treePropsConfig" :node-key="nodeKey" highlight-current @node-click="handleNodeClick" />
+        <el-tree
+          ref="tree"
+          :data="treeData"
+          :props="treePropsConfig"
+          :node-key="nodeKey"
+          :highlight-current="true"
+          @node-click="handleNodeClick"
+        />
       </el-option>
     </el-select>
   </div>
@@ -16,26 +23,27 @@ export default {
   name: 'TreeSelect',
   components: {},
   props: {
-    // 树形数据，必须传递
+    // 树形数据，必须传递，由父组件提供
     treeData: {
       type: Array,
       required: true
     },
+    // 父组件传入的值对象，必须传递，用于更新下拉框的选中值
     value: {
       type: Object,
       required: true
     },
-    // 占位符文本，默认为“请选择”
+    // 下拉框的占位符文本，默认为“请选择”
     placeholder: {
       type: String,
       default: '请选择'
     },
-    // 节点的唯一标识key
+    // 节点的唯一标识key，默认使用 'id' 作为标识字段
     nodeKey: {
       type: String,
       default: 'id'
     },
-    // el-tree 组件的配置项
+    // el-tree 组件的配置项，用于配置节点标签和子节点字段
     treePropsConfig: {
       type: Object,
       default: () => ({
@@ -46,17 +54,25 @@ export default {
   },
   data() {
     return {
-      label: ''
+    }
+  },
+  computed: {
+    label() {
+      return this.value.name || ''
     }
   },
   watch: {
-    // 监听父组件传入的值的变化，并同步label
     value: {
-      // 当组件初始化时立即执行一次
       immediate: true,
       handler(newVal) {
-        console.log('val', newVal)
-        this.label = newVal.name
+        this.$nextTick(() => {
+          setTimeout(() => {
+            if (this.$refs.tree) {
+              // 设置高亮显式
+              this.$refs.tree.setCurrentKey(newVal.id)
+            }
+          }, 300) // 可以调整延迟时间
+        })
       }
     }
   },
